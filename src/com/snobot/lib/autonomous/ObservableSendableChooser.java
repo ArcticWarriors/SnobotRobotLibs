75baa5f96083d21a88e9fa8fe78873b6d6a64a9a
PJ Reiniger <pj.reiniger@gmail.com>
2018-01-11 01:11:28 -0500

    Compile with wpilib 2018

--- src/com/snobot/lib/autonomous/ObservableSendableChooser.java
+++ src/com/snobot/lib/autonomous/ObservableSendableChooser.java
@@ -2,18 +2,21 @@ package com.snobot.lib.autonomous;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.TableEntryListener;
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilderImpl;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
 public class ObservableSendableChooser<V> extends SendableChooser<V>
 {
-    private String mSendableName;
     private NetworkTable mTable;
 
     @Override
-    public void initTable(NetworkTable table)
+    public void initSendable(SendableBuilder builder)
     {
-        mTable = table;
-        super.initTable(table);
+        super.initSendable(builder);
+
+        SendableBuilderImpl castBuilder = (SendableBuilderImpl) builder;
+        mTable = castBuilder.getTable();
     }
 
     public void addSelectionChangedListener(TableEntryListener listener)
